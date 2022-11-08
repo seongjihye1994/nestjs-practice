07a21338e628003e80921155870f1dbdf1707593
jihye.seong <jihye.seong@ipageon.com>
2022-11-08 16:25:19 +0900

    user entity의 username에 유니크한 데이터 insert 처리하기

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['username']) // 중복되는 username 데이터는 들어갈 수 없게 처리
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
