fd4c414b8d5bbe8a4fb64cf8bd30368ab6339cbd
jihye.seong <jihye.seong@ipageon.com>
2022-11-10 13:58:13 +0900

    환경변수 설정

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     private userRepository: Repository<User>,
   ) {
     super({
-      secretOrKey: 'Secret1234', // 토큰이 유효한 지 체크할 때 사용하는 시크릿키
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'), // 토큰이 유효한 지 체크할 때 사용하는 시크릿키
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 토큰을 어디에서 가져올지 -> header, 토큰 타입은 무엇인지 -> bearer
     });
   }
