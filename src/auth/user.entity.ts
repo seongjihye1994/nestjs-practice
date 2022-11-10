3ba46ba958c91b08e5dcf37a197a2ee8a91671c4
jihye.seong <jihye.seong@ipageon.com>
2022-11-10 09:38:28 +0900

    유저 객체와 게시판 객체의 연관관계 설정, 유저 -> 게시판 OneToMany, 게시판 -> 유저 ManyToOne 설정

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,7 +1,9 @@
+import { Board } from 'src/boards/board.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
@@ -17,4 +19,12 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  // 회원과 게시판의 연관관계 설정!
+  // 회원은 여러개의 게시물 등록 가능 => OneToMany
+  // type => Board : 가지고 올 타입은 Board 타입
+  // board => board.user : board에서 user를 접근하려면 board.user로
+  // {eager: true} : 패치 전략은 eager (user를 가져올 때 board도 가져옴)
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
